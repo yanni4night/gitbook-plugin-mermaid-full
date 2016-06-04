f34ad58032480e92e3b41439301e58dfde19de44
yinyong02 <yinyong02@baidu.com>
2016-06-04 14:43:45 +0800

    done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gitbook-plugin-mermaid-full
-Mermaid plugin for gitbook
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url]
+
+Mermaid plugin for gitbook.
+
+[npm-url]: https://npmjs.org/package/gitbook-plugin-mermaid-full
+[downloads-image]: http://img.shields.io/npm/dm/gitbook-plugin-mermaid-full.svg
+[npm-image]: http://img.shields.io/npm/v/gitbook-plugin-mermaid-full.svg
---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+  * Copyright (C) 2016 yanni4night.com
+  * index.js
+  *
+  * changelog
+  * 2016-06-04[14:38:15]:revised
+  *
+  * @author yanni4night@gmail.com
+  * @version 1.0.0
+  * @since 1.0.0
+  */
+module.exports = {
+    book: {
+        assets: "./book",
+        js: [
+            "mermaidAPI.min.js",
+            "plugin.js"
+        ],
+        css: [
+            "mermaidAPI.css"
+        ],
+        html: {
+        }
+    }
+};
